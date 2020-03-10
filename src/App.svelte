4eaa8d31213199ac9799c0b6f9e8be9ff4d7b582
Tifeh <subairbarakat@gmail.com>
2020-03-10 23:13:45 +0100

    basic setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,56 @@
 <script>
-	export let name;
+	let visible = false;
+
+	function typewriter(node, { speed = 50 }) {
+		const valid = (
+				node.childNodes.length === 1 &&
+				node.childNodes[0].nodeType === 3
+		);
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`);
+		}
+
+		const text = node.textContent;
+		const duration = text.length * speed;
+
+		return {
+			duration,
+			tick: t => {
+				const i = ~~(text.length * t);
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
 </script>
 
+<label>
+	<input type="checkbox" bind:checked={visible}>
+	visible
+</label>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<nav style="background-color: #f3f3f3">
+		<ul>
+			<li>Overview </li>
+			<li>Features </li>
+			<li>Video </li>
+			<li>Reviews </li>
+			<li>Screenshots </li>
+			<li>Overview </li>
+		</ul>
+	</nav>
+{#if visible}
+	<h1 in:typewriter>Show your mobile apps with James!</h1>
+	<p in:typewriter>It's a mobile app landing page designed with material design in core. Present your app the best way it deserves.</p>
+{/if}
 </main>
 
 <style>
+	:global(body) {
+		background-image: url(https://i0.wp.com/www.techgrapple.com/wp-content/uploads/2016/09/Black-Wallpaper-in-2K-Resolution.jpg?fit=2560%2C1440&ssl=1);
+		background-color: #616266;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -15,12 +58,36 @@
 		margin: 0 auto;
 	}
 
+
+
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #996600;
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+	p{
+		color: #996600;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		float: right;
+		list-style-type: none;
+		margin: 0 3vw;
+	}
+
+	nav{
+		width: 100vw;
+		color: #996600;
+	}
+
+
 
 	@media (min-width: 640px) {
 		main {
